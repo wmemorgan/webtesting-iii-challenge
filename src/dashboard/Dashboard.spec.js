b01edd1bc511b0d3f7faa12679abc962bc239ae7
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-27 01:23:10 +0800

    Add Gate default tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import { render } from 'react-testing-library'
+import { render, fireEvent, cleanup } from 'react-testing-library'
 import renderer from 'react-test-renderer'
 
 import Dashboard from './Dashboard'
 import Display from '../display/Display'
 import Controls from '../controls/Controls'
 
+afterEach(cleanup)
+
 describe('<Dashboard />', () => {
   it('matches snapshort', () => {
     const tree = renderer.create(<Dashboard/>)
     expect(tree.toJSON()).toMatchSnapshot()
+  })
+})
+
+describe('application defaults', () => {
+  it('defaults to unlocked and open', () => {
+    render(<Dashboard />).getByText(/unlocked/i)
+    render(<Dashboard />).getByText(/open/i)
+  })
+
+  it('cannot be closed or opened if it is locked', () => {
+    const toggleCloseTest = jest.fn()
+    let mockState = {
+      locked: true,
+      closed: true,
+      toggleClosed: toggleCloseTest
+    }
+  
+    const controlsInstance = render(<Controls {...mockState} />)
+    fireEvent.click(controlsInstance.getByText(/open gate/i))
+    expect(toggleCloseTest).not.toHaveBeenCalled()
   })
 })
 
