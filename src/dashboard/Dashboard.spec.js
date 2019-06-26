ca47d03ab2ace90ddce2886437a2123754d58af0
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-26 21:50:55 +0800

    Add child component render tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,20 @@
-// Test away
+import React from 'react';
+import { render } from 'react-testing-library'
+import Dashboard from './Dashboard'
+import Display from '../display/Display'
+import Controls from '../controls/Controls'
+
+describe('Test Dashboard component', () => {
+  it('renders without crashing', () => {
+    render(<Dashboard/>)
+  })
+
+  it('Display component renders', () => {
+    render(<Display />)
+  })
+
+  it('Controls component renders', () => {
+    render(<Controls/>)
+  })
+
+})
