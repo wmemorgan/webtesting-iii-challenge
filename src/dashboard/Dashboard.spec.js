e6a9a4a255e2ebc7d32800d25ce3092f4a42d878
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-26 21:51:16 +0800

    Add snapshot test

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { render } from 'react-testing-library'
+import renderer from 'react-test-renderer'
+
 import Dashboard from './Dashboard'
 import Display from '../display/Display'
 import Controls from '../controls/Controls'
 
-describe('Test Dashboard component', () => {
+describe('<Dashboard />', () => {
+  it('matches snapshort', () => {
+    const tree = renderer.create(<Dashboard/>)
+    expect(tree.toJSON()).toMatchSnapshot()
+  })
+})
+
+describe('Child components render', () => {
   it('renders without crashing', () => {
     render(<Dashboard/>)
   })
